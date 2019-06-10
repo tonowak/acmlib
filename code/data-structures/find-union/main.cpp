6cfef346e4285792c7adcced618c653c313fc252
Tomasz Nowak <36604952+tonowak@users.noreply.github.com>
2019-06-10 18:57:07 +0200

    Added constructor to find&union

--- code/data-structures/find-union/main.cpp
+++ code/data-structures/find-union/main.cpp
@@ -1,13 +1,16 @@
+// status: untested
 struct FindUnion {
 	vector<int> rep;
-	bool same_set(int a, int b) { return find(a) == find(b); }
+	bool sameSet(int a, int b) { return find(a) == find(b); }
 	int size(int x) { return -rep[find(x)]; }
 	int find(int x) { return e[x] < 0 ? x : e[x] = find(e[x]); }
-	void join(int a, int b) {
+	bool join(int a, int b) {
 		a = find(a), b = find(b);
-		if(a == b) return;
+		if(a == b) return false;
 		if(-rep[a] < -rep[b]) swap(a, b);
 		rep[a] += rep[b];
 		rep[b] = a;
+		return true;
 	}
+	FindUnion(int n) : rep(n, -1) {}
 };
