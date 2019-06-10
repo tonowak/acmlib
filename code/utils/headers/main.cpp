79e1d34d079f57b89cd14180efb21b51f6e626b6
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2019-06-10 16:11:10 +0200

    Added headers

---
+++ code/utils/headers/main.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+using L = long long;
+#define FOR(i, l, r) for(int i = (l); i <= (r); ++i)
+#define REP(i, n) FOR(i, 0, (n) - 1)
+template<class A, class B> ostream& operator<<(ostream &out, const pair<A, B> &p) {
+	return out << '(' << p.first << ", " << p.second << ')';
+}
+template<class T> auto operator<<(ostream &out, T &&x) -> decltype(x.begin(), out) {
+	out << '{';
+	for(auto it = x.begin(); it != x.end(); ++it)
+		out << *it << (it == --x.end() ? "" : ", ");
+	return out << '}';
+}
+void dump() {}
+template<class T, class... Args> void dump(T &&x, Args... args) {
+	cerr << x << ";  ";
+	dump(args...);
+}
+#ifdef DEBUG
+  const int seed = 1;
+  struct Nl{~Nl(){cerr << '\n';}};
+# define debug(x...) cerr << (#x != "" ? #x ":  " : ""), dump(x), Nl(), cerr
+#else
+  const int seed = chrono::system_clock::now().time_since_epoch().count();
+# define debug(...) 0 && cerr
+#endif
+mt19937_64 rng(seed);
+int rd(int l, int r) {
+	return uniform_int_distribution<int>(l, r)(rng);
+}
+// end of templates
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+}
