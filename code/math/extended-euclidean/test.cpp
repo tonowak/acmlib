10ab6a1f64ce777621198c3d45c70d28748cafae
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2019-06-14 22:48:37 +0200

    Added test-wrapper.cpp

--- code/math/extended-euclidean/test.cpp
+++ code/math/extended-euclidean/test.cpp
@@ -1,13 +1,19 @@
 #include "main.cpp"
+#include "../../utils/testing/test-wrapper.cpp"
 
-int main() {
-	REP(i, 1000) {
-		LL limit = LL(1e18);
-		LL a = rd(-limit, limit), b = rd(-limit, limit);
-		LL good_gcd = __gcd(a, b);
-		LL gcd, x, y;
-		tie(gcd, x, y) = extendedGcd(a, b);
-		assert(gcd == good_gcd);
-		// assert(a * x + b * y == gcd);
-	}
+bool test(int) {
+	int test_type = rd(0, 1);
+	LL limit = test_type ? LL(1e18) : LL(1e9);
+
+	LL a = rd(-limit, limit), b = rd(-limit, limit);
+	LL good_gcd = __gcd(a, b);
+	LL gcd, x, y;
+	tie(gcd, x, y) = extendedGcd(a, b);
+	assert(gcd == good_gcd);
+
+	if(test_type == 0)
+		assert(a * x + b * y == gcd);
+	debug(a, b, x, y, gcd);
+	assert(abs(x) < abs(b) && abs(y) < abs(a));
+	return false;
 }
