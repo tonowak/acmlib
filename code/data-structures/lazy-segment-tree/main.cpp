eb4c073128d3ceba5af22b41ec7021deafcdf812
staniewzki <m.staniewzki@gmail.com>
2019-06-15 12:30:28 +0200

    Fixed lazy segment tree

--- code/data-structures/lazy-segment-tree/main.cpp
+++ code/data-structures/lazy-segment-tree/main.cpp
@@ -1,5 +1,5 @@
 // Drzewo Przedział Przedział
-// Status : Nieprzetestowane
+// Status : Przetestowane
 // add(l, r, val) dodaje na przedziale
 // quert(l, r) bierze maxa z przedziału
 
@@ -13,24 +13,26 @@ struct Tree {
 	int size = 1;
 
 	Tree(int n) {
-		while(n < size) size *= 2;
+		while(size < n) size *= 2;
 		nodes.resize(size * 2);
 		for(int i = size - 1; i >= 1; i--)
 			nodes[i].size = nodes[i * 2].size * 2;
 	}
 
-	void propagate(int v)
-	{
-		nodes[v].val = max(nodes[v].val, nodes[v].lazy);
+	void add_val(int v, int val) {
+		nodes[v].val += val;
+		nodes[v].lazy += val;
+	}
+
+	void propagate(int v) {
+		REP(i, 2) add_val(v * 2 + i, nodes[v].lazy);
 		nodes[v].lazy = 0;
-		if(nodes[v].size == 1) return;
-		REP(i, 2) nodes[v * 2 + i].lazu += nodes[v].lazy;
 	}
 
 	int query(int l, int r, int v = 1) {
-		propagate(v);	
 		if(l == 0 && r == nodes[v].size - 1)
 			return nodes[v].val;
+		propagate(v);	
 		int m = nodes[v].size / 2;
 		if(r < m)
 			return query(l, r, v * 2);
@@ -41,11 +43,11 @@ struct Tree {
 	}
 
 	void add(int l, int r, int val, int v = 1) {
-		propagate(v);
 		if(l == 0 && r == nodes[v].size - 1) {
-			nodes[v].lazy += val;
+			add_val(v, val);
 			return;
 		}
+		propagate(v);
 		int m = nodes[v].size / 2;
 		if(r < m)
 			add(l, r, val, v * 2);
@@ -53,6 +55,7 @@ struct Tree {
 			add(l - m, r - m, val, v * 2 + 1);
 		else
 			add(l, m - 1, val, v * 2), add(0, r - m, val, v * 2 + 1);
+
 		nodes[v].val = max(nodes[v * 2].val, nodes[v * 2 + 1].val);
 	}
 };
