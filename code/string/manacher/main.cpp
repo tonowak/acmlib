cfad174defb077b50e315283e14fbc6539f7e103
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2019-07-30 10:06:23 +0200

    Added Manacher

---
+++ code/string/manacher/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * Opis: radius[p][i] = $rad$ = największy promień palindromu
+ *   parzystości p o środku i. $L=i-rad+!p, \; R=i+rad$ to palindrom
+ *   dla [abaababaab] daje [003000020], [0100141000]
+ * Czas: O(n)
+ */
+
+array<vector<int>, 2> manacher(vector<int> &in) {
+	int n = size(in);
+	array<vector<int>, 2> radius = {{vector<int>(n - 1), vector<int>(n)}};
+	REP(parity, 2) {
+		int z = parity ^ 1, L = 0, R = 0;
+		REP(i, n - z) {
+			int &rad = radius[parity][i];
+			if(i <= R - z)
+				rad = min(R - i, radius[parity][L + (R - i - z)]);
+			int l = i - rad + z, r = i + rad;
+			while(0 <= l - 1 && r + 1 < n && in[l - 1] == in[r + 1])
+				++rad, ++r, --l;
+			debug(i, rad, l, r);
+			if(r > R)
+				L = l, R = r;
+		}
+	}
+	return radius;
+}
---
+++ code/string/manacher/test.cpp
@@ -0,0 +1,28 @@
+#include "../../utils/testing/test-wrapper.cpp"
+#include "main.cpp"
+
+bool test(int) {
+	int n = rd(1, 10);
+	vector<int> in(n);
+	for(int &x : in)
+		x = rd(0, 1);
+
+	array<vector<int>, 2> radius = {vector<int>(n - 1), vector<int>(n)};
+	REP(l, n)
+		FOR(r, l, n - 1) {
+			vector<int> s;
+			FOR(i, l, r)
+				s.emplace_back(in[i]);
+			vector<int> cp = s;
+			reverse(cp.begin(), cp.end());
+			if(cp != s)
+				continue;
+
+			int mid = (l + r) / 2, len = r - l + 1;
+			radius[len % 2][mid] = max(radius[len % 2][mid], len / 2);
+		}
+
+	if(radius != manacher(in))
+		return true;
+	return false;
+}
