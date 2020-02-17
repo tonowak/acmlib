381aa28873e619a1e58f43d82d53cb27b18d692c
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2020-02-17 14:11:27 +0100

    tested FIO

--- code/optimizations/fio/test.cpp
+++ code/optimizations/fio/test.cpp
@@ -1,8 +1,9 @@
-#include "main.cpp"
 #include "../../utils/testing/test-wrapper.cpp"
+#include "main.cpp"
 
 bool test(int) {
 	freopen("t.in", "r", stdin);
 	for(int i = 10; i > 0; --i)
 		assert(fastin() == i);
+	return false;
 }
