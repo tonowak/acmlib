7459b6be7f6b57e6f68eb7fb2863729e378d1118
Tomasz Nowak <36604952+tonowak@users.noreply.github.com>
2021-08-02 18:42:52 +0200

    Dodanie opcji podwójnego hashowania w hashowaniu

--- code/string/hashing/main.cpp
+++ code/string/hashing/main.cpp
@@ -14,20 +14,26 @@ struct Hashing {
 	int mod = 1e9 + 696969;
 	int base;
 
-	Hashing(string &str, int b = -1) {
-		if(b == -1) base = my_rd(30, 50);
-		else base = b;
-
+	Hashing(string &str, int b) {
+		base = b;
 		int len = ssize(str);
 		ha.resize(len + 1);
 		pw.resize(len + 1, 1);
 		REP(i, len) {
-			ha[i + 1] = ((LL) ha[i] * base + str[i] - 'a' + 1) % mod;
-			pw[i + 1] = ((LL) pw[i] * base) % mod;
+			ha[i + 1] = int(((LL) ha[i] * base + str[i] - 'a' + 1) % mod);
+			pw[i + 1] = int(((LL) pw[i] * base) % mod);
 		}
 	}
 
 	int operator()(int l, int r) {
-		return ((ha[r + 1] - (LL) ha[l] * pw[r - l + 1]) % mod + mod) % mod;
+		return int(((ha[r + 1] - (LL) ha[l] * pw[r - l + 1]) % mod + mod) % mod);
+	}
+};
+
+struct DoubleHashing {
+	Hashing h1, h2;
+	DoubleHashing(string &str) : h1(str, 31), h2(str, 33) {} // change to rd on codeforces
+	LL operator()(int l, int r) {
+		return h1(l, r) * LL(h2.mod) + h2(l, r);
 	}
 };
