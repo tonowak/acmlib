a59cfb2b1a4b870b4687e6242d7101a0e54b7451
Arkadiusz Czarkowski <arkadiusz.czarkowski2212@gmail.com>
2021-11-03 18:58:33 +0100

    Rewritten part of useful.tex

--- code/math/floor-sum/main.cpp
+++ code/math/floor-sum/main.cpp
@@ -1,6 +1,6 @@
 /*
  * Opis: Liczy $\sum_{i=0}^{n-1} \left\lfloor \frac{a\cdot i + b}{c} \right\rfloor$
- * Czas: O(\log(c))
+ * Czas: O(\log(a))
  * Użycie: floor_sum(n, a, b, c)
  * Działa dla $0 \leq a, b < c$ oraz $1\leq c, n \leq 10^9$.
  * Dla innych $n, a, b, c$ trzeba uważać lub użyć __int128.
