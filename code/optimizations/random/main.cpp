0e7e8d99fa5e66a1b20d82b7047bcbcc4c905b91
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2022-03-14 17:04:28 +0100

    Dodano szybsze rand

---
+++ code/optimizations/random/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * Opis: Szybsze rand.
+ */
+ 
+uint32_t xorshf96() {
+	static uint32_t x = 123456789, y = 362436069, z = 521288629;
+	uint32_t t;
+	x ^= x << 16;
+	x ^= x >> 5;
+	x ^= x << 1;
+	t = x;
+	x = y;
+	y = z;
+	z = t ^ x ^ y;
+	return z;
+}
