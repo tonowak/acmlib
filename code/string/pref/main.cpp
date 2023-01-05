bbea523c9cb16988fbe8ef1a986ddbcd837f941b
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2023-01-05 18:31:58 +0100

    Usunięcie zduplikowanego z-function

--- code/string/pref/main.cpp
+++ code/string/pref/main.cpp
@@ -5,15 +5,17 @@
  */
 
 vector<int> pref(vector<int> str) {
-	int len = ssize(str);	
-	vector<int> ret(len);
-	ret[0] = len;
+	int n = ssize(str);
+	vector<int> ret(n);
+	ret[0] = n;
 	int i = 1, m = 0;
-	while(i < len) {
-		while(m + i < len && str[m + i] == str[m]) m++;
+	while(i < n) {
+		while(m + i < n and str[m + i] == str[m])
+			m++;
 		ret[i++] = m;
-		m = (m != 0 ? m - 1 : 0);
-		for(int j = 1; ret[j] < m; m--) ret[i++] = ret[j++];
+		m = max(0, m - 1);
+		for(int j = 1; ret[j] < m; m--)
+			ret[i++] = ret[j++];
 	}
 	return ret;
 }
