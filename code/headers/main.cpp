348ceff06f235a5a6b3c00a5cb85611d2656bf10
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2023-02-18 23:06:11 +0100

    heh zamieniłem ostream& na auto&

--- code/headers/main.cpp
+++ code/headers/main.cpp
@@ -7,8 +7,8 @@ using LL=long long;
 #define FOR(i,l,r) for(int i=(l);i<=(r);++i)
 #define REP(i,n) FOR(i,0,(n)-1)
 #ifdef DEBUG
-auto&operator<<(ostream&o,pair<auto,auto>p){return o<<'('<<p.first<<", "<<p.second<<')';}
-auto operator<<(ostream&o,auto x)->decltype(x.end(),o){o<<'{';REP(i,ssize(x))o<<","[!i]<<x[i];return o<<'}';}
+auto&operator<<(auto&o,pair<auto,auto>p){return o<<'('<<p.first<<", "<<p.second<<')';}
+auto operator<<(auto&o,auto x)->decltype(x.end(),o){o<<'{';REP(i,ssize(x))o<<","[!i]<<x[i];return o<<'}';}
 #define debug(X...) cerr<<"["#X"]: ",[](auto...$){((cerr<<$<<"; "),...);}(X),cerr<<'\n'
 #else
 #define debug(...) {}
