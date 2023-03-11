449ef77101328f4ddcd864bda8901de851fc5df5
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2023-03-11 22:37:47 +0100

    Dodanie wyznaczanie przedziałów podsłów w suffix array

---
+++ code/string/suffix-array-interval/main.cpp
@@ -0,0 +1,33 @@
+/*
+ * Opis: Wyznaczanie przedziałów podsłowa w tablicy suffixowej
+ * Czas: O(t \log n)
+ * Użycie: zwraca przedział [l, r], gdzie dla każdego i w [l, r], t jest podsłowem sa.sa[i],
+ *   lub [-1, -1] jeżeli nie ma takiego i.
+ */
+#include "../suffix-array/main.cpp"
+
+pair<int, int> get_substring_sa_range(const vector<int> &s, const SuffixArray &sa, const vector<int> &t) {
+	auto get_lcp = [&](int i) -> int {
+		REP(k, ssize(t))
+			if(i + k >= ssize(s) or s[i + k] != t[k])
+				return k;
+		return ssize(t);
+	};
+	auto get_side = [&](bool search_left) {
+		int l = 0, r = ssize(sa.sa) - 1;
+		while(l < r) {
+			int m = (l + r + not search_left) / 2, lcp = get_lcp(sa.sa[m]);
+			if(lcp == ssize(t))
+				(search_left ? r : l) = m;
+			else if(sa.sa[m] + lcp >= ssize(s) or s[sa.sa[m] + lcp] < t[lcp])
+				l = m + 1;
+			else
+				r = m - 1;
+		}
+		return l;
+	};
+	int l = get_side(true);
+	if(get_lcp(sa.sa[l]) != ssize(t))
+		return {-1, -1};
+	return {l, get_side(false)};
+}
---
+++ code/string/suffix-array-interval/test.cpp
@@ -0,0 +1,49 @@
+#include "../../utils/testing/test-wrapper.cpp"
+#include "main.cpp"
+
+void test() {
+	int n = rd(1, 30);
+	int r = rd(1, 26);
+	vector<int> s(n);
+	for (auto& x : s)
+		x = rd(0, r - 1);
+
+	int m = rd(1, n + 1);
+	vector<int> t;
+	if(m <= n and rd(0, 1)) {
+		int l = rd(0, n - m);
+		REP(i, m)
+			t.emplace_back(s[l + i]);
+	}
+	else {
+		t.resize(m);
+		for (auto& x : t)
+			x = rd(0, r - 1);
+	}
+
+	SuffixArray sa(s);
+
+	auto get_t_lcp = [&](int i) -> int {
+		REP(k, ssize(t))
+			if(i + k >= n)
+				return k;
+			else if(s[i + k] != t[k])
+				return k;
+		return ssize(t);
+	};
+
+	vector<int> good_positions;
+	REP(i, ssize(sa.sa))
+		if(get_t_lcp(sa.sa[i]) == ssize(t))
+			good_positions.emplace_back(i);
+	debug(n, s, t, sa.sa, good_positions);
+	REP(i, ssize(good_positions) - 1)
+		assert(good_positions[i] + 1 == good_positions[i + 1]);
+
+	auto [found_l, found_r] = get_substring_sa_range(s, sa, t);
+	debug(found_l, found_r);
+	if(good_positions.empty())
+		assert(found_l == -1 and found_r == -1);
+	else
+		assert(found_l == good_positions.front() and found_r == good_positions.back());
+}
