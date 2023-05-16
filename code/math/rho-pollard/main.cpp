b5714783c18c03addcf66420dc791753f7fde428
Markadiusz <arkadiusz.czarkowski2212@gmail.com>
2023-05-16 23:30:59 +0200

    Added all_factors to rho-pollard

--- code/math/rho-pollard/main.cpp
+++ code/math/rho-pollard/main.cpp
@@ -2,7 +2,9 @@
  * Opis: Rozkład na czynniki Rho Pollarda
  * Czas: O(n^{\frac{1}{4}})
  * Użycie: factor(n) zwraca vector dzielników pierwszych n, niekoniecznie posortowany
+ * all_factors(n) zwraca vector wszystkich dzielników n, niekoniecznie posortowany
  * factor(12) = {2, 2, 3}, factor(545423) = {53, 41, 251};
+ * all_factors(12) = {1, 3, 2, 6, 4, 12}
  */
 
 #include "../miller-rabin/main.cpp"
@@ -25,4 +27,26 @@ vector<LL> factor(LL n) {
 	auto l = factor(x), r = factor(n / x);
 	l.insert(l.end(), r.begin(), r.end());
 	return l;
+}
+
+vector<LL> all_factors(LL n) {
+	auto v = factor(n);
+	sort(v.begin(), v.end());
+	vector<LL> ret;
+	function<void(LL,int)> gen = [&](LL val, int p) {
+		if (p > ssize(v)) {
+			ret.emplace_back(val);
+			return;
+		}
+		int cnt = 1;
+		while (p < ssize(v) and v[p] == v[p - 1])
+			++p, ++cnt;
+		gen(val, p + 1);
+		REP(i, cnt) {
+			val *= v[p - 1];
+			gen(val, p + 1);
+		}
+	};
+	gen(1, 1);
+	return ret;
 }
--- code/math/rho-pollard/test.cpp
+++ code/math/rho-pollard/test.cpp
@@ -2,22 +2,44 @@
 #include "main.cpp"
 
 void test() {
-	auto f = [&](LL n) {
-		vector<LL> v;
-		for (LL d = 2; d * d <= n; ++d) {
-			while (n % d == 0) {
-				n /= d;
-				v.emplace_back(d);
+	{ // factor test
+		auto f = [&](LL n) {
+			vector<LL> v;
+			for (LL d = 2; d * d <= n; ++d) {
+				while (n % d == 0) {
+					n /= d;
+					v.emplace_back(d);
+				}
 			}
-		}
-		if (n > 1) v.emplace_back(n);
-		return v;
-	};
-	LL mn = (1ll << 40);
-	LL n = rd_ll(1, mn);
-	vector<LL> vrho = factor(n);
-	sort(vrho.begin(), vrho.end());
-	vector<LL> vbrute = f(n);
-	sort(vbrute.begin(), vbrute.end());
-	assert(vrho == vbrute);
+			if (n > 1) v.emplace_back(n);
+			return v;
+		};
+		LL mn = (1ll << 40);
+		LL n = rd_ll(1, mn);
+		vector<LL> vrho = factor(n);
+		sort(vrho.begin(), vrho.end());
+		vector<LL> vbrute = f(n);
+		sort(vbrute.begin(), vbrute.end());
+		assert(vrho == vbrute);
+	}
+	{ // all_factors test
+		auto f = [&](LL n) {
+			vector<LL> v;
+			for (LL d = 1; d * d <= n; ++d) {
+				if (n % d == 0) {
+					v.emplace_back(d);
+					if (d != n / d)
+						v.emplace_back(n / d);
+				}
+			}
+			return v;
+		};
+		LL mn = (1ll << 40);
+		LL n = rd_ll(1, mn);
+		auto vrho = all_factors(n);
+		sort(vrho.begin(), vrho.end());
+		auto vbrute = f(n);
+		sort(vbrute.begin(), vbrute.end());
+		assert(vrho == vbrute);
+	}
 }
