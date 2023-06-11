97d0fa22b12ee148e9bed97c2fe8a00cec9ea779
Markadiusz <arkadiusz.czarkowski2212@gmail.com>
2023-06-11 18:49:26 +0200

    Expanded matching's test.cpp

--- code/flow/matching/test.cpp
+++ code/flow/matching/test.cpp
@@ -37,7 +37,7 @@ bool is_valid_matching(int n, vector<pair<int, int>> edges, vector<int> match) {
 }
 
 void test() {
-	int n0 = rd(1, 7), n1 = rd(1, 7);
+	int n0 = rd(0, 7), n1 = rd(0, 7);
 	int n = n0 + n1;
 	vector<int> which_group(n0 + n1);
 	REP(i, n1)
@@ -47,7 +47,7 @@ void test() {
 	REP(v, n)
 		in_group[which_group[v]].emplace_back(v);
 
-	int m = min(15, rd(0, n0 * n1 + 1));
+	int m = min(15, rd(0, n0 * n1));
 	vector<pair<int, int>> edges(m);
 	for(auto &[v, u] : edges) {
 		v = in_group[0][rd(0, n0 - 1)];
