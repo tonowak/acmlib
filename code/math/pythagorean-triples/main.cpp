db6aad49413742c1e15683ea1fe9dea5d5828ca8
Markadiusz <arkadiusz.czarkowski2212@gmail.com>
2023-07-27 09:17:35 +0200

    Small fix

--- code/math/pythagorean-triples/main.cpp
+++ code/math/pythagorean-triples/main.cpp
@@ -1,7 +1,6 @@
 /*
  * Opis: Wyznacza wszystkie trójki $(a, b, c)$ takie, że $a^2+b^2=c^2$, $gcd(a, b, c) = 1 oraz $c \leq \text{limit}$. Zwraca tylko jedną z $(a, b, c)$ oraz $(b, a, c)$.
  */
-
 vector<tuple<int, int, int>> pythagorean_triples(int limit) {
 	vector<tuple<int, int, int>> ret;
 	function<void(int, int, int)> gen = [&](int a, int b, int c) {
