0ed8d7df5b11e1fb0571fc6a1070da4af2eb1352
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2023-08-02 19:43:04 +0100

    Fix typo

--- code/math/xor-base/main.cpp
+++ code/math/xor-base/main.cpp
@@ -1,20 +1,20 @@
 /*
  * Opis: O(nB+B^2) dla $B=bits$, dla $S$ wyznacza minimalny zbiór $B$ taki, że każdy element $S$ można zapisać jako xor jakiegoś podzbioru $B$.
  */
-int hightest_bit(int ai) {
+int highest_bit(int ai) {
 	return ai == 0 ? 0 : __lg(ai) + 1;
 }
 constexpr int bits = 30;
 vector<int> xor_base(vector<int> elems) {
 	vector<vector<int>> at_bit(bits + 1);
 	for(int ai : elems)
-		at_bit[hightest_bit(ai)].emplace_back(ai);
+		at_bit[highest_bit(ai)].emplace_back(ai);
 	for(int b = bits; b >= 1; --b)
 		while(ssize(at_bit[b]) > 1) {
 			int ai = at_bit[b].back();
 			at_bit[b].pop_back();
 			ai ^= at_bit[b].back();
-			at_bit[hightest_bit(ai)].emplace_back(ai);
+			at_bit[highest_bit(ai)].emplace_back(ai);
 		}
 	at_bit.erase(at_bit.begin());
 	REP(b0, bits - 1)
