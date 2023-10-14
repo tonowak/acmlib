30463daba85d3af5911b8dbc54f13d757d4a794b
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2023-10-14 17:43:52 +0100

    Shortened hashing

--- code/string/hashing/main.cpp
+++ code/string/hashing/main.cpp
@@ -4,26 +4,21 @@
 struct Hashing {
 	vector<LL> ha, pw;
 	static constexpr LL mod = (1ll << 61) - 1;
+	LL reduce(LL x) { return x >= mod ? x - mod : x; }
 	LL mul(LL a, LL b) {
 		const auto c = __int128(a) * b;
-		LL val = LL(c & mod) + LL(c >> 61);
-		if (val >= mod) val -= mod;
-		return val;
+		return reduce(LL(c & mod) + LL(c >> 61));;
 	}
 	Hashing(const vector<int> &str, const int base = 37) {
 		int len = ssize(str);
 		ha.resize(len + 1);
 		pw.resize(len + 1, 1);
 		REP(i, len) {
-			LL val = mul(ha[i], base) + str[i] + 1;
-			if (val >= mod) val -= mod;
-			ha[i + 1] = val;
+			ha[i + 1] = reduce(mul(ha[i], base) + str[i] + 1);
 			pw[i + 1] = mul(pw[i], base);
 		}
 	}
 	LL operator()(int l, int r) {
-		LL val = ha[r + 1] - mul(ha[l], pw[r - l + 1]) + mod;
-		if (val >= mod) val -= mod;
-		return val;
+		return reduce(ha[r + 1] - mul(ha[l], pw[r - l + 1]) + mod);
 	}
 };
