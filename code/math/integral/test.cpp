5f1de00e64ab3001b1abb2a19aa776d0e2c0447a
Markadiusz <arkadiusz.czarkowski2212@gmail.com>
2023-10-14 22:50:20 +0200

    Improved integrate testing

--- code/math/integral/test.cpp
+++ code/math/integral/test.cpp
@@ -2,16 +2,16 @@
 #include "main.cpp"
 
 void test() {
-	int a = rd(-5, 5), b = rd(-5, 5), c = rd(-5, 5);
+	int a = rd(-5, 5), b = rd(-5, 5), c = rd(-5, 5), d = rd(-5, 5), e = rd(-5, 5);
 	auto f = [&](D x) {
-		return x * x * a + x * b + c;
+		return x * x * a + x * b + c + d * sin(x) + e * exp(x);
 	};
 	D l = rd(-5, 5), r = rd(-5, 5);
 	if(l > r)
 		swap(l, r);
 	D wzorc = integrate(f, l, r);
 	auto fp = [&](D x) {
-		return x * x * x * a / 3 + x * x * b / 2 + x * c;
+		return x * x * x * a / 3 + x * x * b / 2 + x * c - d * cos(x) + e * exp(x);
 	};
 	D brute = fp(r) - fp(l);
 	assert(abs(wzorc - brute) < 1e-8);
