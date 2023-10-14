0d4deb3c86595c4787c5a8fe5531197ef11cf8c3
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2023-10-14 18:15:37 +0100

    shorten hull

--- code/geo/convex-hull/main.cpp
+++ code/geo/convex-hull/main.cpp
@@ -1,39 +1,25 @@
 /* 
- * Opis: O(n \log n), \texttt{top\_bot\_hull} zwraca osobno górę i dół po id,
- *   \texttt{hull\_id} zwraca całą otoczkę po id,
- *   \texttt{hull} zwraca punkty na otoczce.
+ * Opis: O(n \log n), \texttt{top\_bot\_hull} zwraca osobno górę i dół,
+ *   \texttt{hull} zwraca punkty na otoczce clockwise gdzie pierwszy jest najbardziej lewym.
  */
 #include "../point/main.cpp"
-int cross(P a, P b, P c) { return sign(cross(b - a, c - a)); }
-pair<vector<int>, vector<int>> top_bot_hull(const vector<P> &pts) {
-	int n = ssize(pts);
-	vector<int> ord(n), top, bot;
-	REP(i, n) ord[i] = i;
-	sort(ord.begin(), ord.end(), [&](int i, int j) {
-		return pts[i] < pts[j];
-	});
-	REP(dir, 2) {
-		vector<int> &hull = (dir ? bot : top);
-		auto l = [&](int i) { return pts[hull.end()[-i]]; };
-		for(int i : ord) {
-			while(ssize(hull) > 1 && cross(l(2), l(1), pts[i]) >= 0)
-				hull.pop_back();
-			hull.emplace_back(i);
+array<vector<P>, 2> top_bot_hull(vector<P> in) {
+	sort(in.begin(), in.end());
+	array<vector<P>, 2> ret;
+	REP(d, 2) {
+		for(auto p : in) {
+			while(ssize(ret[d]) > 1 and dir(ret[d].end()[-2], ret[d].back(), p) >= 0)
+				ret[d].pop_back();
+			ret[d].emplace_back(p);
 		}
-		reverse(ord.begin(), ord.end());
+		reverse(in.begin(), in.end());
 	}
-	return {top, bot};
-}
-vector<int> hull_id(const vector<P> &pts) {
-	if(pts.empty()) return {};
-	auto [top, bot] = top_bot_hull(pts);
-	top.pop_back(), bot.pop_back();
-	top.insert(top.end(), bot.begin(), bot.end());
-	return top;
-}
-vector<P> hull(const vector<P> &pts) {
-	vector<P> ret;
-	for(int i : hull_id(pts))
-		ret.emplace_back(pts[i]);
 	return ret;
+}
+vector<P> hull(vector<P> in) {
+	if(ssize(in) <= 2) return in;
+	auto ret = top_bot_hull(in);
+	REP(d, 2) ret[d].pop_back();
+	ret[0].insert(ret[0].end(), ret[1].begin(), ret[1].end());
+	return ret[0];
 }
