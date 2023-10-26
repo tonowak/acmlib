81d23a56ca411207cfd3c5d2bdc6f3d1890e0fdf
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2023-10-26 09:33:04 +0100

    Reduce Point to work only on D

--- code/geo/point/test.cpp
+++ code/geo/point/test.cpp
@@ -4,9 +4,9 @@ using LL = long long;
 
 #include "main.cpp"
 
-void f([[maybe_unused]] Point<LL> p) {}
+void f([[maybe_unused]] P p) {}
 
-ostream& operator<<(ostream& os, Point<LL> p) {
+ostream& operator<<(ostream& os, P p) {
 	return os << "(" << p.x << ", " << p.y << ")\n";
 }
 
@@ -15,34 +15,34 @@ void assert_eq(D a, D b) {
 }
 
 int main() {
-	Point<LL> p = {2, 3};
+	P p = {2, 3};
 	// assign operator
-	p = Point<LL>{6, 9};
-	assert(p.x == 6 && p.y == 9);
+	p = {6, 9};
+	assert(equal(p.x, 6) && equal(p.y, 9));
 
 	p.x = 5;
 	p.y = 3;
-	assert(p.x == 5 && p.y == 3);
+	assert(equal(p.x, 5) && equal(p.y, 3));
 
-	Point squared = p * p;
+	P squared = p * p;
 	// (5 + 3i) * (5 + 3i) = 25 - 9 + 30i = (16, 30)
-	assert(squared.x == 16 && squared.y == 30);
+	assert(equal(squared.x, 16) && equal(squared.y, 30));
 
 	// implicit casting std::complex -> Point
 	f(p * p);
 	ofstream o("/dev/null");
 	o << p * p << "\n";
 
-	p = Point<LL>{3, 4};
+	p = P{3, 4};
 	// assert(abs(p) == 5); this fails... dont use ints
 
-	assert_eq(abs(Point<double>{3, 4}), 5);
+	assert_eq(abs(P{3, 4}), 5);
 	assert_eq(cross({1, 0}, {0, 1}), 1);
 
 	auto lambda = [&]{ return p; };
 	auto q = lambda();
 	q.x += 1;
-	assert(p.x + 1 == q.x);
+	assert(equal(p.x + 1, q.x));
 
 	cout << "OK\n";
 }
