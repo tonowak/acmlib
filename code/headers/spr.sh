b541dd44d959e427c9743f68fccec09c654588a5
Markadiusz <arkadiusz.czarkowski2212@gmail.com>
2023-10-28 21:00:32 +0200

    Shortened spr

--- code/headers/spr.sh
+++ code/headers/spr.sh
@@ -2,10 +2,6 @@ for ((i=0;;i++)); do
 	./gen < g.in > t.in
 	./main < t.in > m.out
 	./brute < t.in > b.out
-	if diff -w m.out b.out > /dev/null; then
-		printf "OK $i\r"
-	else
-		echo WA
-		return 0
-	fi
+	printf "OK $i\r"
+	diff -wq m.out b.out || break
 done
