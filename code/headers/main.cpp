fd95e2774a35cb9a28e54b923a6aa67c263331eb
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2023-11-06 22:33:54 +0100

    Remove unnecessary spaces in main.cpp headers, part 2

--- code/headers/main.cpp
+++ code/headers/main.cpp
@@ -4,9 +4,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 using LL=long long;
-#define FOR(i,l,r) for(int i=(l);i<=(r);++i)
-#define REP(i,n) FOR(i,0,(n)-1)
-#define ssize(x) int(x.size())
+#define FOR(i,l,r)for(int i=(l);i<=(r);++i)
+#define REP(i,n)FOR(i,0,(n)-1)
+#define ssize(x)int(x.size())
 #ifdef DEBUG
 auto&operator<<(auto&o,pair<auto,auto>p){return o<<"("<<p.first<<", "<<p.second<<")";}
 auto&operator<<(auto&o,auto x){o<<"{";int i=0;for(auto e:x)o<<","+!i++<<e;return o<<"}";}
@@ -16,4 +16,6 @@ auto&operator<<(auto&o,auto x){o<<"{";int i=0;for(auto e:x)o<<","+!i++<<e;return
 #endif
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
+	REP(i, 5)
+		debug(i);
 }
