d2534c798d8b985600a2c2e3bd90281eecb8f25a
Markadiusz <arkadiusz.czarkowski2212@gmail.com>
2023-12-05 15:11:38 +0100

    Restored old memoryusage.cpp

---
+++ code/headers/memoryusage.cpp
@@ -0,0 +1,7 @@
+/*
+ * Opis: Trzeba wywołać pod koniec main'a. Uwzględnia również \texttt{unused capacity} pochodzące np. z \texttt{std::vector::reverse}.
+ */
+
+#ifdef LOCAL
+system("grep VmPeak /proc/$PPID/status >&2");
+#endif
