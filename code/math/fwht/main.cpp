3c3ba76fba48edc4ec1681645403c2f5be6e3593
BCzarkowski <bartczarkowski@gmail.com>
2024-01-07 19:25:41 +0100

    Skrócone FWHT, ale z beznadziejną stałą

--- code/math/fwht/main.cpp
+++ code/math/fwht/main.cpp
@@ -11,93 +11,49 @@
  *   \texttt{ifwht\_xor(fwht\_xor(a)) == a},
  *   \texttt{convolution\_xor(a, b)[i] = suma(j \^ k == i) a[j] * b[k]}.
  */
+
+#include "../simple-modulo/main.cpp"
+
 // BEGIN HASH
-vector<int> fwht_or(vector<int> a) {
+using T = array<array<int, 2>, 2>;
+void kronecker(vector<int> &a, T mt) {
 	int n = ssize(a);
 	assert((n & (n - 1)) == 0);
-	for(int s = 1; 2 * s <= n; s *= 2)
-		for(int l = 0; l < n; l += 2 * s)
-			for(int i = l; i < l + s; ++i)
-				a[i + s] += a[i];
-	return a;
-}
-vector<int> ifwht_or(vector<int> a) {
+	for (int s = 1; 2 * s <= n; s *= 2)
+		for (int l = 0; l < n; l += 2 * s)
+			for (int i = l; i < l + s; ++i) {
+				int val = a[i];
+				a[i] = add(mul(a[i], mt[0][0]), mul(a[i + s], mt[0][1]));
+				a[i + s] = add(mul(val, mt[1][0]), mul(a[i + s], mt[1][1]));
+			}
+} // END HASH
+// BEGIN HASH
+vector<int> kronecker_convolution(vector<int> a, vector<int> b, T mt, T rev) {
 	int n = ssize(a);
-	assert((n & (n - 1)) == 0);
-	for(int s = n / 2; s >= 1; s /= 2)
-		for(int l = 0; l < n; l += 2 * s)
-			for(int i = l; i < l + s; ++i)
-				a[i + s] -= a[i];
+	assert(ssize(b) == n);
+	kronecker(a, mt);
+	kronecker(b, mt);
+	REP (i, n)
+		a[i] = mul(a[i], b[i]);
+	kronecker(a, rev);
 	return a;
-}
+} // END HASH
+// BEGIN HASH
 vector<int> convolution_or(vector<int> a, vector<int> b) {
-	int n = ssize(a);
-	assert((n & (n - 1)) == 0 and ssize(b) == n);
-	a = fwht_or(a);
-	b = fwht_or(b);
-	REP(i, n)
-		a[i] *= b[i];
-	return ifwht_or(a);
+	return kronecker_convolution(a, b,
+			T{{{1, 0}, {1, 1}}},
+			T{{{1, 0}, {mod - 1, 1}}});
 } // END HASH
 // BEGIN HASH
-vector<int> fwht_and(vector<int> a) {
-	int n = ssize(a);
-	assert((n & (n - 1)) == 0);
-	for(int s = 1; 2 * s <= n; s *= 2)
-		for(int l = 0; l < n; l += 2 * s)
-			for(int i = l; i < l + s; ++i)
-				a[i] += a[i + s];
-	return a;
-}
-vector<int> ifwht_and(vector<int> a) {
-	int n = ssize(a);
-	assert((n & (n - 1)) == 0);
-	for(int s = n / 2; s >= 1; s /= 2)
-		for(int l = 0; l < n; l += 2 * s)
-			for(int i = l; i < l + s; ++i)
-				a[i] -= a[i + s];
-	return a;
-}
 vector<int> convolution_and(vector<int> a, vector<int> b) {
-	int n = ssize(a);
-	assert((n & (n - 1)) == 0 and ssize(b) == n);
-	a = fwht_and(a);
-	b = fwht_and(b);
-	REP(i, n)
-		a[i] *= b[i];
-	return ifwht_and(a);
+	return kronecker_convolution(a, b,
+			T{{{1, 1}, {0, 1}}},
+			T{{{1, mod - 1}, {0, 1}}});
 } // END HASH
 // BEGIN HASH
-vector<int> fwht_xor(vector<int> a) {
-	int n = ssize(a);
-	assert((n & (n - 1)) == 0);
-	for(int s = 1; 2 * s <= n; s *= 2)
-		for(int l = 0; l < n; l += 2 * s)
-			for(int i = l; i < l + s; ++i) {
-				int t = a[i + s];
-				a[i + s] = a[i] - t;
-				a[i] += t;
-			}
-	return a;
-}
-vector<int> ifwht_xor(vector<int> a) {
-	int n = ssize(a);
-	assert((n & (n - 1)) == 0);
-	for(int s = n / 2; s >= 1; s /= 2)
-		for(int l = 0; l < n; l += 2 * s)
-			for(int i = l; i < l + s; ++i) {
-				int t = a[i + s];
-				a[i + s] = (a[i] - t) / 2;
-				a[i] = (a[i] + t) / 2;
-			}
-	return a;
-}
 vector<int> convolution_xor(vector<int> a, vector<int> b) {
-	int n = ssize(a);
-	assert((n & (n - 1)) == 0 and ssize(b) == n);
-	a = fwht_xor(a);
-	b = fwht_xor(b);
-	REP(i, n)
-		a[i] *= b[i];
-	return ifwht_xor(a);
+	const int rev2 = (mod + 1) / 2;
+	return kronecker_convolution(a, b,
+			T{{{1, 1}, {1, mod - 1}}},
+			T{{{rev2, rev2}, {rev2, mod - rev2}}});
 } // END HASH
--- code/math/fwht/test.cpp
+++ code/math/fwht/test.cpp
@@ -6,24 +6,8 @@ vector<int> get_random_a(int n = -1) {
 		n = 1 << rd(0, 4);
 	vector<int> a(n);
 	for(int &ai : a)
-		ai = rd(-5, 5);
+		ai = rd(0, mod - 1);
 	return a;
-}
-
-void test_fwht_or() {
-	vector<int> a = get_random_a();
-	int n = ssize(a);
-	vector<int> should_be(n);
-	REP(i, n)
-		REP(j, n)
-			if((j | i) == i)
-				should_be[i] += a[j];
-	assert(should_be == fwht_or(a));
-}
-
-void test_ifwht_or() {
-	vector<int> a = get_random_a();
-	assert(ifwht_or(fwht_or(a)) == a);
 }
 
 void test_convolution_or() {
@@ -35,24 +19,8 @@ void test_convolution_or() {
 		REP(j, n)
 			REP(k, n)
 				if((j | k) == i)
-					should_be[i] += a[j] * b[k];
+					should_be[i] = add(should_be[i], mul(a[j], b[k]));
 	assert(should_be == convolution_or(a, b));
-}
-
-void test_fwht_and() {
-	vector<int> a = get_random_a();
-	int n = ssize(a);
-	vector<int> should_be(n);
-	REP(i, n)
-		REP(j, n)
-			if((j & i) == i)
-				should_be[i] += a[j];
-	assert(should_be == fwht_and(a));
-}
-
-void test_ifwht_and() {
-	vector<int> a = get_random_a();
-	assert(ifwht_and(fwht_and(a)) == a);
 }
 
 void test_convolution_and() {
@@ -64,26 +32,8 @@ void test_convolution_and() {
 		REP(j, n)
 			REP(k, n)
 				if((j & k) == i)
-					should_be[i] += a[j] * b[k];
+					should_be[i] = add(should_be[i], mul(a[j], b[k]));
 	assert(should_be == convolution_and(a, b));
-}
-
-void test_fwht_xor() {
-	vector<int> a = get_random_a();
-	int n = ssize(a);
-	vector<int> should_be(n);
-	REP(i, n)
-		REP(j, n)
-			if(__builtin_popcount(i & j) % 2 == 0)
-				should_be[i] += a[j];
-			else
-				should_be[i] -= a[j];
-	assert(should_be == fwht_xor(a));
-}
-
-void test_ifwht_xor() {
-	vector<int> a = get_random_a();
-	assert(ifwht_xor(fwht_xor(a)) == a);
 }
 
 void test_convolution_xor() {
@@ -95,18 +45,12 @@ void test_convolution_xor() {
 		REP(j, n)
 			REP(k, n)
 				if((j ^ k) == i)
-					should_be[i] += a[j] * b[k];
+					should_be[i] = add(should_be[i], mul(a[j], b[k]));
 	assert(should_be == convolution_xor(a, b));
 }
 
 void test() {
-	test_fwht_or();
-	test_ifwht_or();
 	test_convolution_or();
-	test_fwht_and();
-	test_ifwht_and();
 	test_convolution_and();
-	test_fwht_xor();
-	test_ifwht_xor();
 	test_convolution_xor();
 }
