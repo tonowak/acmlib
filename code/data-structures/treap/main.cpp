8ea8b65724c9f04c7e933a1ffeaeaedff825a125
BCzarkowski <bartczarkowski@gmail.com>
2024-03-05 14:49:45 +0100

    Implicit Treap rework

--- code/data-structures/treap/main.cpp
+++ code/data-structures/treap/main.cpp
@@ -1,45 +1,46 @@
 /*
  * Opis: O(\log n) Implict Treap,
  *   wszystko indexowane od 0,
- *   \texttt{insert(i, val)} insertuję na pozycję $i$,
- *   \texttt{treap[i]} zwraca $i$-tą wartość.
+ *   do Node dopisujemy jakie chcemy mieć trzymać dodatkowo dane.
+ *   Jeśli chcemy robić lazy, to wykonania push należy wstawić
+ *   tam gdzie oznaczono komentarzem.
  */
-mt19937 rng_key(0);
-struct Treap {
+namespace Treap {
+	mt19937 rng_key(0);
 	struct Node {
-		int prio, val, cnt;
+		int prio, cnt;
 		Node *l = nullptr, *r = nullptr;
-		Node(int _val) : prio(int(rng_key())), val(_val) {}
+		Node() : prio(int(rng_key())) {}
 		~Node() { delete l; delete r; }
 	};
 	using pNode = Node*;
-	pNode root = nullptr;
-	~Treap() { delete root; }
-	int cnt(pNode t) { return t ? t->cnt : 0; }
+	int get_cnt(pNode t) { return t ? t->cnt : 0; }
 	void update(pNode t) {
-		if(!t) return;
-		t->cnt = cnt(t->l) + cnt(t->r) + 1;
+		if (!t) return;
+		t->cnt = get_cnt(t->l) + get_cnt(t->r) + 1;
 	}
 	void split(pNode t, int i, pNode &l, pNode &r) {
-		if(!t) l = r = nullptr;
-		else if(i <= cnt(t->l))
+		if (!t) {
+			l = r = nullptr;
+			return;
+		}
+		// push(t);
+		if (i <= get_cnt(t->l))
 			split(t->l, i, l, t->l), r = t;
 		else
-			split(t->r, i - cnt(t->l) - 1, t->r, r), l = t;
+			split(t->r, i - get_cnt(t->l) - 1, t->r, r), l = t;
 		update(t);
 	}
 	void merge(pNode &t, pNode l, pNode r) {
-		if(!l || !r) t = (l ? l : r);
-		else if(l->prio > r->prio)
+		if (!l || !r) t = (l ? l : r);
+		else if (l->prio > r->prio) {
+			// push(l);
 			merge(l->r, l->r, r), t = l;
-		else
+		}
+		else {
+			// push(r);
 			merge(r->l, l, r->l), t = r;
+		}
 		update(t);
 	}
-	void insert(int i, int val) {
-		pNode t;
-		split(root, i, root, t);
-		merge(root, root, new Node(val));
-		merge(root, root, t);
-	}
-};
+}
